7bfec83e4241cdf2bcb61b7f795fcb2ab461f438
Jurica Jurincic <juricajurincic1@gmail.com>
2021-02-18 17:56:51 +0100

    :boom: initialization resolves #4

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+<div class="container">
+  <h1 class="text-center">Studentchain</h1>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  components: {}
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,8 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+//Bootstrap js and css import
+import 'bootstrap'
+import './assets/app.scss'
+
+createApp(App).mount('#app')
