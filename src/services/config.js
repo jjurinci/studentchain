46248ae46a0923d5f0c6d5b4c85398ecbb969da4
Jurica Jurincic <juricajurincic1@gmail.com>
2021-02-23 02:49:32 +0100

    :bug: http -> https fixed

--- src/services/config.js
+++ src/services/config.js
@@ -1,6 +1,6 @@
 const config = {
     //dmlUrl : 'http://localhost:3000'
-    dmlUrl : 'http://studentchain-alpha-backend.herokuapp.com'
+    dmlUrl : 'https://studentchain-alpha-backend.herokuapp.com'
 }
 
 export default config
